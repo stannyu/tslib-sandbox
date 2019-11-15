41ab18aaf830843593588d1f1c8abd938cf701b9
stanislauy <stas.yudzitski@yandex.ru>
2019-11-15 11:54:04 +0300

    Unnecessary files removed.

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "cl" {
-    export class Cl {
-        static clMethod(): void;
-    }
-}
-declare module "index" {
-    export function justSumNums(a: number, b: number): number;
-}
-declare module "sum.spec" { }
